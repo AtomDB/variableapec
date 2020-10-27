50101e74a115bb614fa6747c65177c60e8be91d6
keriheuer <54902150+keriheuer@users.noreply.github.com>
2020-10-27 19:18:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,46 +1,9 @@
 from setuptools import setup, Extension
-# from https://gist.github.com/dcreager/300803 with "-dirty" support added
-import os, variableapec
-
-linapprox =  Extension("linear_approx",['linear_approx.c'],\
-                       define_macros = [('MAJOR_VERSION', '1'),\
-                                        ('MINOR_VERSION','0')])
-
-def GetGitVersion():
-    '''report the git commit/branch/tag on which we are '''
-    repo = Repo(".", search_parent_directories=True)
-    git = repo.git    
-
-    branchOrTag=git.rev_parse('--abbrev-ref', 'HEAD')
-
-    if branchOrTag == 'HEAD':
-        # get tag name
-        # since several tags are sometime put on the same commit we want to retrieve all of them
-        # and use the last one as reference
-        # Note:
-        # branchOrTag=`git describe --tags --exact-match` does not provided the latest created tag in case several point to the same place
-        currentSha=git.rev_parse('--verify','HEAD')
-
-        # list all tags on the current sha using most recent first:
-        allTags=git.tag('--points-at',currentSha,'--sort=-creatordate')
-        print (allTags)
-
-        allTagsArray=allTags.split(' ') #create an array (assuming tags are separated by space)
-
-        # if we checkouted a commit with no tag associated, the allTagsArray is empty we can use directly the sha value
-        if len(allTagsArray) == 0:
-            branchOrTag=git.rev-rev_parse('--short','HEAD') # take the short sha
-        else:
-
-            branchOrTag=allTagsArray[0] #first from the list
-    else:
-        #add the head commit id on the current branch
-        branchOrTag="{}[{}]".format(branchOrTag,git.rev_parse('--short', 'HEAD'))
-
-    return branchOrTag
+from setuptools_scm import get_version
+import os
 
 setup(name='variableapec',
-      version=variableapec.__version__,
+      version=get_version(root=".", relative_to_"variableapec.py"),
       description='variableapec tool for AtomDB python library.',
       url='http://www.atomdb.org',
       author='Keri Heuer',
